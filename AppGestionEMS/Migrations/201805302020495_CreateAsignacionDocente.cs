42a0c396073631be181ef436b57cce21f75efdb9
anthonywillismunoz <anthony.willis.munoz@alumnos.upm.es>
2018-05-30 22:36:40 +0200

    Agregar archivos de proyecto.

---
+++ AppGestionEMS/Migrations/201805302020495_CreateAsignacionDocente.cs
@@ -0,0 +1,35 @@
+namespace AppGestionEMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateAsignacionDocente : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.AsignacionDocentes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CursoId = c.Int(nullable: false),
+                        GrupoClaseId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Cursos", t => t.CursoId, cascadeDelete: true)
+                .ForeignKey("dbo.GrupoClases", t => t.GrupoClaseId, cascadeDelete: true)
+                .Index(t => t.CursoId)
+                .Index(t => t.GrupoClaseId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AsignacionDocentes", "GrupoClaseId", "dbo.GrupoClases");
+            DropForeignKey("dbo.AsignacionDocentes", "CursoId", "dbo.Cursos");
+            DropIndex("dbo.AsignacionDocentes", new[] { "GrupoClaseId" });
+            DropIndex("dbo.AsignacionDocentes", new[] { "CursoId" });
+            DropTable("dbo.AsignacionDocentes");
+        }
+    }
+}
---
+++ AppGestionEMS/Models/AsignacionDocentes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppGestionEMS.Models
+{
+    public class AsignacionDocentes
+    {
+        public int Id { get; set; }
+        [Display(Name="Curso")]
+        public int CursoId { get; set; }
+        public virtual Cursos Curso { get; set; }
+        [Display(Name="GrupoClase")]
+        public int GrupoClaseId { get; set; }
+        public virtual GrupoClase GrupoClase { get; set; }
+    }
+}
